11e7d820ad370ed47d53eee31737bba4aa30022c
arturk22 <alencar.artur00@gmail.com>
2022-04-12 16:32:25 -0300

    separando responsabilidades

--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -1,32 +1,36 @@
-class tabelas {
-    init(conexao){
+class Tabelas {
+    init(conexao) {
         this.conexao = conexao
+
         this.criarAtendimentos()
         this.criarPets()
     }
 
-    criarAtendimentos(){
-        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20) NOT NULL, servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL,status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
-        this.conexao.query(sql, (erro) => {
-            if(erro){
+    criarAtendimentos() {
+        const sql =
+            'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(11) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
+
+        this.conexao.query(sql, erro => {
+            if (erro) {
                 console.log(erro)
-            }else {
-                console.log('Tabela atendimentos criada com sucesso.')
+            } else {
+                console.log('Tabela Atendimentos criada com sucesso')
             }
         })
     }
 
-    criarPets(){
-       const query = 'CREATE TABLE IF NOT EXISTS Pets (id int NOT NULL AUTO_INCREMENT, NOME VARCHAR(50), imagem varchar(200), PRIMARY KEY(id))'
-    
-       this.conexao.query(query, erro => {
-           if(erro){
-               console.log(erro)
-           }else{
-               console.log('Tabela Pets foi criada com sucesso')
-           }
-       })
+    criarPets() {
+        const query =
+            'CREATE TABLE IF NOT EXISTS Pets (id int NOT NULL AUTO_INCREMENT, nome varchar(50), imagem varchar(200), PRIMARY KEY (id))'
+
+        this.conexao.query(query, erro => {
+            if (erro) {
+                console.log(erro)
+            } else {
+                console.log('Tabela Pets foi criada com sucesso')
+            }
+        })
     }
 }
 
-module.exports = new tabelas
+module.exports = new Tabelas()

5b37616bc578f4ef189b7ab4124ce134f71e5bb6
arturk22 <alencar.artur00@gmail.com>
2022-04-12 12:13:42 -0300

    manipulação de arquivos

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,5 +1,21 @@
 const fs = require('fs')
 
-fs.createReadStream('./assets/salsicha.jpg')
-    .pipe(fs.createReadStream('./assets/salsicha-stream.jpg'))
-    .on('finish', () => console.log('Imagem foi escrita com sucesso'))
+module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
+
+    const tiposValidos = ['jpg', 'png', 'jpeg']
+    const tipo = path.extname(caminho)
+    const tipoEhValido = tiposValidos.indexOf(tipo.substring(1)) !== -1
+    if(tipoEhValido){
+        const erro = "Tipo é invalido"
+        console.log("Erro! Tipo invalido")
+        callbackImagemCriada(erro)
+    }else{
+        const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`
+    
+        fs.createReadStream(caminho)
+            .pipe(fs.createReadStream(novoCaminho))
+            .on('finish', () => callbackImagemCriada(false,novoCaminho))
+    
+    }
+
+}
--- models/pets.js
+++ models/pets.js
@@ -1,17 +1,33 @@
 const conexao = require('../infraestrutura/conexao')
 
+const uploadDeArquivo = require('../arquivos/uploadDeArquivos')
 
 class Pet{
     adiciona(pet, res){
         const query = 'INSERT INTO Pets ?'
-        conexao.query(query, pet, erro =>{
+        
+        uploadDeArquivo(pet.imagem, pet.nome, (erro, novoCaminho) => {
+            
             if(erro){
-                console.log(erro)
-                res.status(400).json(erro)
-            } else{
-                res.status(200).json(pet)
+                res.status(400).json({erro})
+            }else{
+                
+                const novoPet = {nome: pet.nome, imagem: novoCaminho}
+            
+                conexao.query(query, novoPet, erro =>{
+                    if(erro){
+                        console.log(erro)
+                        res.status(400).json(erro)
+                    } else{
+                        res.status(200).json(pet)
+                    }
+                })
+
             }
+      
         })
+        
+        
     }
 }
 
